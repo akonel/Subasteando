f799742c94e4b99e77f1c02162bd9c745ebfeaa0
Aaron Romero <aromero@comparaonline.com>
2012-03-03 14:32:37 -0300

    Vistas: Creación de index y controller.

--- config/routes.rb
+++ config/routes.rb
@@ -45,6 +45,8 @@ Subasteando::Application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
+  get "public/idex"
+  root to: 'public#index'
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
