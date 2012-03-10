bebfe85750f689e47e84258b57929e37fa87c303
Jorge Vargas Silva <jv.vargass@gmail.com>
2012-03-10 12:57:36 -0300

    configuración deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -10,6 +10,25 @@ set :scm, :git
 set :deploy_to, "/home/jorge/subasteando"
 
 server "173.255.215.155", :web, :app, :db, :primary => true
+
+after "deploy:update_code", :bundle_install
+after "deploy:update_code", :compass_compile
+
+namespace :deploy do
+	desc "Restart passenger"
+	task :restart do
+		run "touch #{current_path}/tmp/restart.txt"
+	end
+end
+
+task :bundle_install, :roles => :app do
+	run "cd #{release_path} && bundle install"
+end
+
+task :compass_compile do
+	run "cd #{release_path} && bundle exec compass compile"
+end
+
 # role , "173.255.215.155"                          # This may be the same as your `Web` server
 # role :db,  "173.255.215.155", :primary => true # This is where Rails migrations will run
 # # role :db,  "173.255.215.155"
