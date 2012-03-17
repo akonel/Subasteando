21afb384e860c95b0afb3419408e8656739afc3d
Aaron Romero <aromero@comparaonline.com>
2012-03-17 14:14:35 -0400

    Subasteador: Se configura la ruta para poder acceder a nueva vista en localhost/subasteador.

--- config/routes.rb
+++ config/routes.rb
@@ -47,6 +47,7 @@ Subasteando::Application.routes.draw do
   #   end
   get "public/idex"
   root to: 'public#index'
+  match "/subasteador/:action" => "subasteador#index", :as => :index
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
